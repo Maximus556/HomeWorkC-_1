6a3f47bda4502753d71e01fc1380eb40f46a822f
Maksim <ivanmax556@gmail.com>
2023-02-11 14:36:14 +0400

    Вторая задача

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -5,17 +5,47 @@
 // a = -9 b = -3 -> max = -3
 
 
-Console.Write("Введите первое число: ");
-int firstValue = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int firstValue = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int secondValue = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int secondValue = Convert.ToInt32(Console.ReadLine());
 
-if (firstValue > secondValue)
-{
-    Console.WriteLine("Чиcло " + firstValue + " больше чем " + secondValue);
-}
-else 
-{
-    Console.WriteLine("Чиcло " + secondValue + " больше чем " + firstValue); 
-}
+// if (firstValue > secondValue)
+// {
+//     Console.WriteLine("Чиcло " + firstValue + " больше чем " + secondValue);
+// }
+// else 
+// {
+//     Console.WriteLine("Чиcло " + secondValue + " больше чем " + firstValue); 
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.Write("Введите первое число: ");
+// int firstValue = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int secondValue = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int thridValue = Convert.ToInt32(Console.ReadLine());
+
+// int max = firstValue;
+
+// if (firstValue > max) max = firstValue;
+// if (secondValue > max) max = secondValue;
+// if (thridValue > max) max = thridValue;
+
+// Console.WriteLine("max = " + max);
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
