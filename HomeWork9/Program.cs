68f9187a3746968d59fa70aab514579bd4dd5e83
Maksim <ivanmax556@gmail.com>
2023-03-11 00:35:47 +0400

    ДЗ 9

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Console.Write("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// string PrintNumbers(int start)
+// {
+//     // Базовый
+//     if (start == 1) return start.ToString();
+//     // Рекурсивный
+//     return (start + "," + PrintNumbers(start - 1));
+// }
+
+// Console.WriteLine($"Натуральные числа от {N} до 1:          {PrintNumbers(N)}");
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.Write("Введите число M: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число N: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// int SumNumbers(int firstNumber, int secondNumber)
+// {
+//     int sum = 0;
+//     // Базовый случай - случай выхода из рекурсии
+//     if (firstNumber == secondNumber) return sum = +secondNumber;
+//     // Рекурсивный случай - задание рекурсии (вызов функции внутри функции)
+//     return (sum = (firstNumber + SumNumbers(firstNumber + 1, secondNumber)));
+// }
+
+// Console.WriteLine($"Cумма натуральных элементов в промежутке от {firstNumber} до {secondNumber}: \t {SumNumbers(firstNumber, secondNumber)}");
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int AkkermanFunction(int m, int n)
+// {
+//     if (m == 0) return n + 1;
+//     if (m != 0 && n == 0) return AkkermanFunction(m - 1, 1);
+//     if (m > 0 && n > 0) return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+//     return AkkermanFunction(m, n);
+// }
+
+// Console.WriteLine($"Функция Аккермана для чисел {m} и {n} = {AkkermanFunction(m, n)}");
---
+++ Lessons9/Program.cs
@@ -0,0 +1,118 @@
+﻿
+//ОПЕРАТОРЫ
+// Унарные операторы - 1 операнд
+// а+
+
+// Бинарные операторы -2 операнда
+// b*a
+
+// Постфиксной: "после" а++
+
+// Префиксный оператор: "перед" ++а
+
+
+// int a = 1;
+
+// int prefixIncrement = ++a * a;
+
+// Console.WriteLine(prefixIncrement); // Наибольший приоритет
+
+// int b = 1; 
+// int postfixIncrement = b++ * b; // наименьший приоритет
+// Console.WriteLine(postfixIncrement);
+
+
+// Тернарный оператор условие ? trye : false
+// условие - 1 операнд ? значение для истины - 2 : значение для лжи - 3
+
+// int dreamSalary = int.MaxValue;
+
+// int realSalary = 50000;
+
+// var result = realSalary < dreamSalary ? "Пока что ЗП не такая большая" : "Наша зарплата: 2147483647";
+
+// Console.WriteLine(result);
+
+
+
+// Рекурсия - задаем выход и по ом само решение
+// int Factorial (int n)
+// {
+//     // Базовый случай - случай выхода из рекурсии
+//     if (n == 1) return 1; // Факториал от 1 = 1
+//     // Рекурсивный случай - задание рекурсии (вызов функции внутри функции)
+//     return n * Factorial(n - 1);
+// }
+
+// Console.WriteLine(Factorial(4)); // 4*3*2*1 = 24
+
+
+// Задача 63: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+/// <summary>
+/// Печатаем все натуральные числа в промежутке от 1 до N.
+/// </summary>
+/// <param name="start"> 1 = start</param>
+/// <param name="end"> N = end</param>
+/// <returns></returns>
+string PrintNumbers(int start, int end)
+{
+    // Базовый
+    if (start == end) return start.ToString();
+    // Рекурсивный
+    return (start + "," + PrintNumbers(start + 1, end));
+}
+
+Console.WriteLine(PrintNumbers(1, N));
+
+
+// Задача 67: Напишите программу, которая будет принимать на вход число 
+// и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int SumNumbers(int num)
+// {
+//     // Базовый
+//     if (num == 0) return 0; // К результату добавляется 0
+//     // Рекурсивный случай
+//     return (num % 10 + SumNumbers(num / 10));
+// }
+
+// Console.WriteLine(SumNumbers(number));
+
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B,
+//  и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите степень: ");
+// int power = Convert.ToInt32(Console.ReadLine());
+
+// int GetPower(int number, int power)
+// {
+//     // Базовый случай
+//     if(power == 0) return 1; // Число в 0 степнени = 1
+//     if(power == 1) return number;// Число в первой степени = число
+//     // Рекурсивный
+//     return (number * GetPower(number, power - 1));
+// }
+// Console.WriteLine(GetPower (number, power));
+
+
+
+
+// ДЗ
+
+// задача 66 как факториал
